550ce7682d0c706518552523944267309a48b5f6
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-01 12:14:02 +0530

    Update config.sh

--- Essential-Files/config.sh
+++ Essential-Files/config.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 cp /Essential-Files/index.html /usr/share/index.html
 cp /Essential-Files/jconf.py /conf/jupyter.py
 cp /Essential-Files/jpass.json /root/.jupyter/jupyter_notebook_config.json
