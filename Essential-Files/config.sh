1dfb7bff4351d0458ba9242ed5f1b0816b61edeb
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-01 14:51:20 +0530

    Update config.sh

--- Essential-Files/config.sh
+++ Essential-Files/config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp /Essential-Files/index.html /usr/share/index.html
+mv /Essential-Files/Aria2Mega.nz /Essential-Files/Aria2Mega.ipynb
 cp /Essential-Files/jconf.py /conf/jupyter.py
 cp /Essential-Files/jpass.json /root/.jupyter/jupyter_notebook_config.json
