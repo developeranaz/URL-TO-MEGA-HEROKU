91682f7582fe8bde8da3793163f717c680eeaaa8
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-01 17:20:23 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+mv /Essential-Files/Aria2Mega.htpy  /Essential-Files/Aria2Mega.ipynb
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
-
+curl 'https://raw.githubusercontent.com/developeranaz/URL-TO-MEGA-HEROKU/main/donate.developeranaz.txt'
 voila --port=$PORT --no-browser  /Essential-Files/Aria2Mega.ipynb --theme=dark
-#jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
