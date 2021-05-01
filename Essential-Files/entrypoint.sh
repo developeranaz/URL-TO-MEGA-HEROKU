20d557c1d2fd22c43c1124e8e36fc492ad2be1a9
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-01 12:17:03 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -1,1 +1,5 @@
-#
+#!/bin/bash
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+jupyter serverextension enable voila --sys-prefix
+
+voila --port=$PORT --no-browser /Aria2.htpy --theme=dark
